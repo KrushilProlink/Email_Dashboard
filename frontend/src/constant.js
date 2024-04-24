ae2604052b51909e4eeb0d2f3649cb7277447b1e
KrushilProlink <krushil.prolink@gmail.com>
2024-04-24 18:00:28 +0530

    change base url

--- frontend/src/constant.js
+++ frontend/src/constant.js
@@ -1,5 +1,6 @@
 export const constant = {
     // baseUrl: 'http://127.0.0.1:5000/'
-    baseUrl: 'https://newinsurance.onrender.com'
+    baseUrl: 'https://newinsurance.onrender.com/'
+    // baseUrl: 'https://insurance-backend-hl7y.onrender.com/'
 }
 
