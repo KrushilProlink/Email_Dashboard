7a39b6d7ca79c7579a729c6363080a12c9860b9e
Rutubhimani28 <rutubhimani.specscale@gmail.com>
2024-05-08 13:01:33 +0530

    Update Sms.js: comment out 'New Sms' button

--- frontend/src/pages/sms/Sms.js
+++ frontend/src/pages/sms/Sms.js
@@ -161,9 +161,9 @@ const Sms = () => {
             <Typography variant="h4">
               Sms List
             </Typography>
-            <Button variant="contained" startIcon={<Iconify icon="eva:plus-fill" />} onClick={handleOpenTask}>
+            {/* <Button variant="contained" startIcon={<Iconify icon="eva:plus-fill" />} onClick={handleOpenTask}>
               New Sms
-            </Button>
+            </Button> */}
 
           </Stack>
           <Box width="100%" >
